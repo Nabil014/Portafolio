df4373209326ea355401266cd4bbf2b3cf12731f
Nabil <nallis014@gmail.com>
2023-06-09 11:36:01 -0300

    change about description

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -21,26 +21,22 @@ const About = () => {
             color='#FF8C72'
             show={true}
             multiline={true}
-            // padding={[20, 20, 20]}
           >
             <span className='span-circle'> Nabil </span>
           </RoughNotation>
-          , argentino, con experiencia en el mundo del desarrollo web y la
-          tecnología.
+          , desarrollador
           <RoughNotation
             animate={true}
             type='highlight'
             color='#FF8C72'
             show={true}
             multiline={true}
-            padding={[60, 20, 20, 60]}
           >
             <span className='span-circle'> Full Stack Developer </span>
           </RoughNotation>
-          y actualmente me estoy capacitando para ser Frontend pero sin
-          descuidar el Backend. Mejorando día a día como programador para
-          enfrentar nuevos desafíos. Me considero responsable, comprometido y
-          busco ser el mejor desarrollador del mundo!!!
+          argentino. Especializado en crear y diseñar aplicaciones web que
+          luzcan geniales, con una buena experiencia de usuario y con diseño
+          responsive para todos los dispositivos.
         </p>
       </div>
     </div>
